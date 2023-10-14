b10eb067dc1a21b83bc493bea564772a9a4bc665
sanjivanihospital204 <sanjivanihospital204@gmail.com>
2023-10-14 15:55:44 +0530

    update the api in client

--- client/src/Services/api.js
+++ client/src/Services/api.js
@@ -2,15 +2,15 @@ import { getLocalStorageObject } from "./util";
 import CryptoJS from 'crypto-js';
 
 export const API_HOSTNAME = 'http://localhost:3007';
-export const VERCEL_API_HOSTNAME = 'https://talk-to-broker.vercel.app';
+export const VERCEL_API_HOSTNAME = 'https://sanjivani-hospital.vercel.app';
 export const LOCAL_OBJECT_SECRET_KEY = 'local_secret_key';
 export const ADMIN_LOGIN_API = '/admin/login';
 export const GET_ALL_PATIENT = '/patient/all';
 export const CREATE_PATIENT = '/patient/create';
 
 export const ADMIN_LOGIN = async (api, data) => {
-  const newApi = API_HOSTNAME + api;
-  // const newApi = VERCEL_API_HOSTNAME + api;
+  // const newApi = API_HOSTNAME + api;
+  const newApi = VERCEL_API_HOSTNAME + api;
   const response = await fetch(newApi, {
     method: 'POST',
     headers: {
@@ -30,8 +30,8 @@ export const POST_API = async (api, data) => {
     user && CryptoJS.AES.decrypt(user, LOCAL_OBJECT_SECRET_KEY).toString(CryptoJS.enc.Utf8);
   const userData = JSON.parse(loggedInUser);
 
-  const newApi = API_HOSTNAME + api;
-  // const newApi = VERCEL_API_HOSTNAME + api;
+  // const newApi = API_HOSTNAME + api;
+  const newApi = VERCEL_API_HOSTNAME + api;
   const response = await fetch(newApi, {
     method: 'POST',
     headers: {
@@ -51,8 +51,8 @@ export const GET_API = async (api) => {
     user && CryptoJS.AES.decrypt(user, LOCAL_OBJECT_SECRET_KEY).toString(CryptoJS.enc.Utf8);
   const userData = JSON.parse(loggedInUser);
 
-  const newApi = API_HOSTNAME + api;
-  // const newApi = VERCEL_API_HOSTNAME + api;
+  // const newApi = API_HOSTNAME + api;
+  const newApi = VERCEL_API_HOSTNAME + api;
   const response = await fetch(newApi, {
     method: 'GET',
     headers: {
